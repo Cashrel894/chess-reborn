514b57c724fd8d8e271a2eecf0e20dee1a2ba2c1
Cashrel894 <cashrel23@sjtu.edu.cn>
2026-02-14 12:08:37 +0800

    feat: update Square's spec & implementation for extended functionality
    Square now supports more powerful indexing and offsetting.
    Also note that Square rep is holding square index now.

--- lib/chess/square.rb
+++ lib/chess/square.rb
@@ -5,37 +5,125 @@ module Chess
   # Represents a single square on a 8*8 Chess board.
   #
   class Square
-    def self.from(obj)
-      return obj if obj.is_a?(self)
+    def self.create(rank, file)
+      file = file.downcase
+      raise ArgumentError unless valid_rank?(rank) && valid_file?(file)
 
-      raise ArgumentError unless obj.is_a?(String) && obj.match(/\A[a-h][1-8]\z/i)
-
-      new(obj[1], obj[0])
+      by_index(rank_to_index(rank), file_to_index(file))
     end
 
-    attr_reader :rank, :file
+    def self.from(obj)
+      return obj if obj.is_a? self
+
+      raise ArgumentError unless obj.is_a?(String) && obj.length == 2
+
+      create(obj[1], obj[0])
+    end
+
+    def rank
+      rank_to_mark(@rank_idx)
+    end
+
+    def file
+      file_to_mark(@file_idx)
+    end
 
     def to_s
-      @file + @rank
+      file + rank
+    end
+
+    def self.by_index(rank_idx, file_idx)
+      rank_idx = rank_idx.to_i
+      file_idx = file_idx.to_i
+
+      raise ArgumentError unless valid_index?(rank_idx) && valid_index?(file_idx)
+
+      new(rank_idx, file_idx)
+    end
+
+    def offset_by(rank_offs, file_offs)
+      rank_offs = rank_offs.to_i
+      file_offs = file_offs.to_i
+
+      new_rank_idx = @rank_idx + rank_offs
+      new_file_idx = @file_idx + file_offs
+
+      return nil unless valid_index?(new_rank_idx) && valid_index?(new_file_idx)
+
+      self.class.by_index(new_rank_idx, new_file_idx)
     end
 
     private
 
     #
     # Rep invariant:
-    #   @rank in '1'..'8', @file in 'a'..'h'
+    #   @rank_idx and @file_idx are both integers and in [0, 7]
     #
     # Abstraction Function:
-    #   AF(@rank, @file) = the square with rank @rank and file @file
+    #   AF(@rank_idx, @file_idx) = the square with double index (@rank_idx, @file_idx)
     #
     # Safety from rep exposure:
     #   All fields are private;
-    #   @rank and @file are both from indexing method and defensively frozen.
+    #   @rank_idx and @file_idx are both integers.
     #
 
-    def initialize(rank, file)
-      @rank = rank.freeze
-      @file = file.freeze
+    def initialize(rank_idx, file_idx)
+      @rank_idx = rank_idx
+      @file_idx = file_idx
     end
+
+    #
+    # Provides private helper methods for Square class.
+    #
+    module SquareHelpers
+      RANK_BASE = '1'
+      FILE_BASE = 'a'
+
+      # arg verifiers
+
+      def valid_index?(index)
+        index.respond_to?(:between?) && index.between?(0, 7)
+      end
+
+      def valid_mark?(mark, base_mark)
+        mark.respond_to?(:ord) && valid_index?(to_index(mark, base_mark))
+      end
+
+      def valid_rank?(mark)
+        valid_mark?(mark, RANK_BASE)
+      end
+
+      def valid_file?(mark)
+        valid_mark?(mark, FILE_BASE)
+      end
+      # transformation methods between mark and index
+
+      def to_index(mark, base_mark)
+        mark.ord - base_mark.ord
+      end
+
+      def to_mark(index, base_mark)
+        (base_mark.ord + index).chr
+      end
+
+      def rank_to_mark(index)
+        to_mark(index, RANK_BASE)
+      end
+
+      def file_to_mark(index)
+        to_mark(index, FILE_BASE)
+      end
+
+      def rank_to_index(mark)
+        to_index(mark, RANK_BASE)
+      end
+
+      def file_to_index(mark)
+        to_index(mark, FILE_BASE)
+      end
+    end
+
+    include SquareHelpers
+    extend SquareHelpers
   end
 end
--- spec/chess/square_spec.rb
+++ spec/chess/square_spec.rb
@@ -3,6 +3,14 @@
 require_relative '../../lib/chess/square'
 
 RSpec.describe Chess::Square do
+  shared_examples 'right square' do |rank, file|
+    it 'returns the right square' do
+      is_expected.to be_a described_class
+      expect(subject.rank).to eq(rank)
+      expect(subject.file).to eq(file)
+    end
+  end
+
   describe '#rank & #file & #to_s' do
     subject(:square) { described_class.from('e4') }
 
@@ -10,6 +18,37 @@ RSpec.describe Chess::Square do
       expect(square.rank).to eq('4')
       expect(square.file).to eq('e')
       expect(square.to_s).to eq('e4')
+    end
+  end
+
+  describe '.create' do
+    subject(:square) { described_class.create(rank, file) }
+
+    context "with lowercase file & file = 'a' & rank = '1'" do
+      let(:rank) { '1' }
+      let(:file) { 'a' }
+
+      include_examples 'right square', '1', 'a'
+    end
+
+    context "with uppercase file & file = 'h' & rank = '8'" do
+      let(:rank) { '8' }
+      let(:file) { 'H' }
+
+      include_examples 'right square', '8', 'h'
+    end
+
+    context "with file in 'b'..'g' & rank = '2'..'7'" do
+      let(:rank) { '4' }
+      let(:file) { 'e' }
+
+      include_examples 'right square', '4', 'e'
+    end
+
+    context 'with invalid input' do
+      it 'raises ArgumentError' do
+        expect { described_class.create('5', 'i') }.to raise_error ArgumentError
+      end
     end
   end
 
@@ -24,46 +63,145 @@ RSpec.describe Chess::Square do
     #       Partition on file: ='a', ='h', in 'b'..'g'
     #
 
+    subject(:square) { described_class.from(obj) }
+
     context 'when obj is a Square' do
       let(:obj) { described_class.from('b5') }
-      subject(:square) { described_class.from(obj) }
 
-      it { is_expected.to be_a described_class }
       it 'returns the same object' do
-        is_expected.to be(obj)
+        is_expected.to be obj
       end
     end
 
     context 'when obj is a String' do
       context "with lowercase file & file = 'a' & rank = '1'" do
-        subject(:square) { described_class.from('a1') }
+        let(:obj) { 'a1' }
 
-        it 'returns a Square object' do
-          is_expected.to be_a described_class
-        end
+        include_examples 'right square', '1', 'a'
       end
 
       context "with uppercase file & file = 'h' & rank = '8'" do
-        subject(:square) { described_class.from('H8') }
+        let(:obj) { 'H8' }
 
-        it 'returns a Square object' do
-          is_expected.to be_a described_class
-        end
+        include_examples 'right square', '8', 'h'
       end
 
       context "with file in 'b'..'g' & rank = '2'..'7'" do
-        subject(:square) { described_class.from('e4') }
+        let(:obj) { 'e4' }
 
-        it 'returns a Square object' do
-          is_expected.to be_a described_class
-        end
+        include_examples 'right square', '4', 'e'
       end
 
       context 'with invalid input' do
+        let(:obj) { 'i5' }
         it 'raises ArgumentError' do
           expect { described_class.from('i5') }.to raise_error ArgumentError
         end
       end
+    end
+  end
+
+  describe '.by_index' do
+    #
+    # Testing Strategy:
+    #   When rank and file are both integers:
+    #     partition on rank: 0, 1-6, 7, invalid
+    #     partition on file: 0, 1-6, 7, invalid
+    #   When rank and file are either non-integer objects that respond to #to_i
+    #
+
+    subject(:square) { described_class.by_index(rank_idx, file_idx) }
+
+    context 'when rank_idx and file_idx are both integers' do
+      context 'with rank_idx = 0 & file_idx = 0' do
+        let(:rank_idx) { 0 }
+        let(:file_idx) { 0 }
+
+        include_examples 'right square', '1', 'a'
+      end
+
+      context 'with rank_idx = 7 & file_idx = 7' do
+        let(:rank_idx) { 7 }
+        let(:file_idx) { 7 }
+
+        include_examples 'right square', '8', 'h'
+      end
+
+      context 'with rank_idx in [2, 6] & file_idx in [2, 6]' do
+        let(:rank_idx) { 3 }
+        let(:file_idx) { 5 }
+
+        include_examples 'right square', '4', 'f'
+      end
+
+      context 'with either rank_idx or file_idx out of range' do
+        let(:rank_idx) { 9 }
+        let(:file_idx) { 3 }
+
+        it 'raises ArgumentError' do
+          expect { described_class.by_index(rank_idx, file_idx) }.to raise_error ArgumentError
+        end
+      end
+    end
+
+    context 'when either rank_idx or file_idx is object that responds to #to_i' do
+      let(:rank_idx) { '3' }
+      let(:file_idx) { '5' }
+
+      include_examples 'right square', '4', 'f'
+    end
+  end
+
+  describe '#offset_by' do
+    #
+    # Testing Strategy:
+    #   When rank_offs and file_offs are both integers:
+    #     partition on rank_offs: <0, =0, >0
+    #     partition on file_offs: <0, =0, >0
+    #     partition on result legality: true, false
+    #   When either rank_offs or file_offs is object that responds to #to_i
+    #
+
+    let(:square) { described_class.from('d4') }
+    subject(:offs) { square.offset_by(rank_offs, file_offs) }
+
+    context 'when rank_offs and file_offs are both integers' do
+      context 'with rank_offs < 0 & file_offs > 0 & result is legal' do
+        let(:rank_offs) { -3 }
+        let(:file_offs) { 2 }
+
+        include_examples 'right square', '1', 'f'
+      end
+
+      context 'with rank_offs > 0 & file_offs = 0 & result is legal' do
+        let(:rank_offs) { 2 }
+        let(:file_offs) { 0 }
+
+        include_examples 'right square', '6', 'd'
+      end
+
+      context 'with rank_offs = 0 & file_offs < 0 & result is legal' do
+        let(:rank_offs) { 0 }
+        let(:file_offs) { -1 }
+
+        include_examples 'right square', '4', 'c'
+      end
+
+      context 'with illegal result' do
+        let(:rank_offs) { 5 }
+        let(:file_offs) { 2 }
+
+        it 'returns nil' do
+          is_expected.to be_nil
+        end
+      end
+    end
+
+    context 'when either rank_offs or file_offs is object that responds to #to_i' do
+      let(:rank_offs) { '0' }
+      let(:file_offs) { '-1' }
+
+      include_examples 'right square', '4', 'c'
     end
   end
 end
